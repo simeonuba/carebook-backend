c42eabe6b929805ab7d53b35fb1ff900990183cc
Simeon Chinweuba <simeonuba@pcs-MacBook-Pro.local>
2022-12-07 23:53:33 -0800

    new json

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,11 @@
 "use strict";
 
 var _express = _interopRequireDefault(require("express"));
-var _index = _interopRequireDefault(require("./index"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var app = (0, _express["default"])();
-(0, _index["default"])(app);
-var PORT = process.env.PORT || 1200;
-app.listen(PORT, function () {
+var _index = _interopRequireDefault(require("./src/index"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+const app = (0, _express.default)();
+(0, _index.default)(app);
+const PORT = process.env.PORT || 1200;
+app.listen(PORT, () => {
   console.log('careBook Server started');
 });
